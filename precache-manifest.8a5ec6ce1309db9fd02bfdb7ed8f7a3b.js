23cf4741e3421c11c6f646f927a20cc34cc375e8
d-d-wolfe <runner@fv-az58.m2lav5rnvobebfqtp2ba0iee4f.cx.internal.cloudapp.net>
2020-07-14 22:05:20 +0000

    Updates

---
+++ precache-manifest.8a5ec6ce1309db9fd02bfdb7ed8f7a3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b51dfb42e568324dd73c67fe26adf90",
+    "url": "/RESTy/index.html"
+  },
+  {
+    "revision": "1171585e25184a0cec8c",
+    "url": "/RESTy/static/css/main.60ea77ac.chunk.css"
+  },
+  {
+    "revision": "fe909692e898bcf33837",
+    "url": "/RESTy/static/js/2.1fd9c230.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RESTy/static/js/2.1fd9c230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1171585e25184a0cec8c",
+    "url": "/RESTy/static/js/main.58b123b8.chunk.js"
+  },
+  {
+    "revision": "9750e7e35bd44a2bd371",
+    "url": "/RESTy/static/js/runtime-main.63213232.js"
+  }
+]);
